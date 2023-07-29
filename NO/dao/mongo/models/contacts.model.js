c8f85f79683a58b253296842a21ac4b3185ebdbe
guillejf <maildeguille@gmail.com>
2023-07-29 11:57:41 -0300

    minor fixes

--- NO/dao/mongo/models/contacts.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-export default mongoose.model('contacts', new mongoose.Schema({
-    first_name: String,
-    last_name: String,
-    age: Number,
-    active: Boolean,
-    phone: String
-}))
--- NO/repositories/index.js
+++
@@ -1,4 +0,0 @@
-import { Contacts } from '../dao/factory.js'
-import ContactRepository from './contacts.repository.js'
-
-export const ContactsService = new ContactRepository(new Contacts())
